8383502419d8f633f700bd9d66591b97e41211ce
harshd3004 <desaiharsh512@gmail.com>
2025-06-05 21:50:14 +0530

    design navbar and mainpage

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return "NOTES"
+    return render_template('main_page.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
