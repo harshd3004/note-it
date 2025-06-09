7fd4effe65555622855310905fd626a73a52a60c
harshd3004 <desaiharsh512@gmail.com>
2025-06-09 08:38:14 +0530

    add update note functionality

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     content = db.Column(db.String(1000), nullable=False)
-    time_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
-    time_modified = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    time_created = db.Column(db.DateTime, default=datetime.now)
+    time_modified = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
 with app.app_context():
     db.create_all()
@@ -30,18 +30,29 @@ def main_page():
 def register():
     return render_template('register.html')
 
-@app.route('/note-editor/',methods=['GET','POST'])
-def noteEditor():
+@app.route('/note-editor',methods=['GET','POST'])
+@app.route('/note-editor/<int:note_id>',methods=['GET','POST'])
+def noteEditor(note_id = None):
+    note = None
+    if note_id:
+        note = Note.query.get(note_id)
     if request.method == "POST":
+        note_id = request.form.get('note_id')
+        if note_id:
+            note = Note.query.get(int(note_id))
         title = request.form['title']
         content = request.form['content']
 
-        new_note = Note(title=title,content=content)
-        db.session.add(new_note)
+        if note:
+            note.title = title
+            note.content = content
+        else : 
+            new_note = Note(title=title,content=content)
+            db.session.add(new_note)
         db.session.commit()
         flash("Changes to note saved successfully", "success")
         return redirect(url_for('main_page'))
-    return render_template('note-editor.html')
+    return render_template('note-editor.html',note=note)
 
 @app.route('/delete-note/<int:id>')
 def delete_note(id):
